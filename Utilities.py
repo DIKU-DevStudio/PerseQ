c8cf04b31e1876392604ac1b224e101043512d49
Patrick-Ranjit D. Madsen <pamad05+github@gmail.com>
2012-03-16 18:06:05 +0100

    Some OMIM tools

--- Utilities.py
+++ Utilities.py
@@ -1,6 +1,36 @@
 from google.appengine.ext import webapp
 from template import jTemplate
 
+from Bio import Entrez
+
+# given list of snpids - returns the list of related OMIM IDs
+def snp_omim(snpids=None):
+	if not snpids:
+		return []
+
+	handle = Entrez.elink(db="omim", dbfrom="snp", id=snpids,linkname="snp_omim")
+	from_dbs = Entrez.read(handle)
+	handle.close()
+
+	omim_ids = [] # holds the id of each of the referened articles        
+    # for each database with references to this snp
+	for from_db in from_dbs:
+		# print db
+		if not from_db.has_key("LinkSetDb"):
+			continue
+        # print db
+        for to_db in from_db["LinkSetDb"]:
+            for link in to_db["Link"]:
+            	omim_ids.append(link['Id'])
+
+	return omim_ids
+
+def omim_efetch(db=None, ids=None):
+	handle = Entrez.efetch(db=db, id=ids, retmode="xml")
+	pubs = Entrez.read(handle)
+	handle.close()
+	print pubs
+
 class AppRequestHandler(webapp.RequestHandler):
     _template = "sample.html"
 
