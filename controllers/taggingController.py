9610768180e39f9f59b313903198576e4d1d0fea
Jacob Wejendorp <jacob@wejendorp.dk>
2012-03-28 19:54:26 +0200

    Fixed tag retrieval/saving

--- controllers/taggingController.py
+++ controllers/taggingController.py
@@ -7,19 +7,30 @@ from models.domain_tag import domain_tag
 class addTag(AppRequestHandler):
     def get(self):
         # Get tags logic
-        snpStr = self.request.get("snp")
-        snpObj = snp.gql("WHERE snpid = '"+snpStr+"'").get()
+        snpStr = self.request.get("snp").lstrip('rsRS')
+        # Get if exists, make dummy if not
+        snpObj = snp.get_by_key_name(snpStr)
+        if snpObj == None:
+            snpObj = snp()
+            snpObj.snpid = int(snpStr)
         tags = domain_tag.all()
-        #tags = snpObj.tags
         self.out({'tags':tags, 'snp':snpObj})
+
     def post(self):
         # Add tag logic
         snpStr = self.request.get("snp")
         tagStr = self.request.get("tag")
-        snpObj = snp.gql('WHERE snpid = '+snpStr).get()
-        tagObj = domain_tag.gql("WHERE tag = '"+tagStr+"'").get()
-        methodStr = self.request.get("method")
 
+        snpObj = snp.get_or_insert(snpStr, snpid=int(snpStr))
+        if snpObj == None:
+            snpObj = snp(snpid = int(snpStr))
+
+        tagObj = domain_tag.get_or_insert(tagStr, tag = tagStr)
+        if tagObj == None:
+            tagObj = tag(tag=tagStr)
+        tagObj.put()
+
+        methodStr = self.request.get("method")
         if methodStr == 'delete':
             snpObj.domain_tags.remove(tagObj.key())
         else:
