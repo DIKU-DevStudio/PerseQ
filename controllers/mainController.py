fa8065e49341c28956777d9f5f9e2048835e9039
Jacob Wejendorp <jacob@wejendorp.dk>
2012-03-28 15:53:23 +0200

    Version 1 of dashboard welcome screen

--- controllers/mainController.py
+++ controllers/mainController.py
@@ -5,11 +5,15 @@
 from Utilities import AppRequestHandler
 from models.snp import snp
 
-
 class snpSearch(AppRequestHandler):
     def get(self):
         snpids = snp.all()
         snpids.order("snpid")
         self.out({'snps':snpids})
 
-__routes__ = [('/', snpSearch)]
+class dashboard(AppRequestHandler):
+    def get(self):
+        self.out()
+
+__routes__ = [('/',dashboard),
+              ('/search/', snpSearch)]
