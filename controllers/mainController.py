a861bfd32db350e678490da088508ae66deb9f67
Jens Raaby <jens+github@m.raaby.co.uk>
2012-05-05 22:08:47 +0200

    Just require logging in for everything...

--- controllers/mainController.py
+++ controllers/mainController.py
@@ -13,7 +13,17 @@ class snpSearch(AppRequestHandler):
 
 class dashboard(AppRequestHandler):
     def get(self):
+        #TODO: If we want to use a login link on the dashboard, this is google's suggested code:
+        # user = users.get_current_user()
+        # if user:
+        #     greeting = ("Welcome, %s! (<a href=\"%s\">sign out</a>)" %
+        #                 (user.nickname(), users.create_logout_url("/")))
+        # else:
+        #     greeting = ("<a href=\"%s\">Sign in or register</a>." %
+        #                 users.create_login_url("/"))
         self.out()
+        
+        
 
 __routes__ = [('/',dashboard),
               ('/search/', snpSearch)]
