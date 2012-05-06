314f87fda235d26485346b5739485ffe1efafa48
Jacob Wejendorp <jacob@wejendorp.dk>
2012-05-06 19:13:25 +0200

    Reduce amount of studies queried due to performance issue

--- controllers/gwasController.py
+++ controllers/gwasController.py
@@ -10,13 +10,13 @@ from models.study import Study
 class gwasReader(AppRequestHandler):
     def get(self):
         self.setTemplate('gwas.html')
-        studies = Study.all().fetch(100,0)
+        studies = Study.all().fetch(10,0)
         self.out({'studies': studies})
 
 class studyPresenter(AppRequestHandler):
     def get(self, i):
         self.setTemplate('study.html')
-        studies = Study.gql('pubmed_id = :1', i).get()
+        studies = Study.gql("WHERE pubmed_id = :1", i).fetch(1,0)
         self.out({'studies': studies})
 
 __routes__ = [('/gwas/', gwasReader),
