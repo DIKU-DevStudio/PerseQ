ad3021aa618bc1843ad793645a97f04c58339f2a
Jens Raaby <jens+github@m.raaby.co.uk>
2012-05-08 17:18:06 +0200

    Working UserData class - creates user object when you load the mainController

--- models/users.py
+++ models/users.py
@@ -1,9 +1,10 @@
 from google.appengine.api import datastore_errors, users
 from google.appengine.ext import db
+import datetime
+
 
 class UserData(db.Model):
-    # derived partly from https://khanacademy.kilnhg.com/Code/Website/Group/stable/File/user_models.py?rev=tip
-
+    # derived from https://khanacademy.kilnhg.com/Code/Website/Group/stable/File/user_models.py?rev=tip
 
     # do not reference this directly
     user = db.UserProperty()
@@ -12,8 +13,6 @@ class UserData(db.Model):
     user_id = db.StringProperty()
 
     user_nickname = db.StringProperty(indexed=False)
-
-    username = db.StringProperty(default="")
 
     moderator = db.BooleanProperty(default=False)
 
@@ -73,8 +72,7 @@ class UserData(db.Model):
         return query.get()
 
     @staticmethod
-    def insert_for(user_id, email,
-                   username=None):
+    def insert_for(user_id, email):
         """Create a user with the specified values, if possible, or returns
         an existing user if the user_id has been used by an existing user.
 
@@ -86,8 +84,7 @@ class UserData(db.Model):
                 to the "key_email" value of UserData, and will also be used
                 for the "email" property, if non-empty. This can be empty
                 for child accounts
-            username:
-                The unique username to set for this user, if any. Optional.
+            
         
         Returns:
             None if user_id or email values are invalid, or the user couldn't
@@ -127,38 +124,14 @@ class UserData(db.Model):
                                 " specified [%s] value" % pname)
             prop_values[pname] = pvalue
 
-        if username:
-            # Username or passwords are separate entities.
-            # That means we have to do this in multiple steps - make a txn.
-            def create_txn():
-                user_data = UserData.get_by_key_name(key_name)
-                if user_data is not None:
-                    logging.warning("Tried to re-make a user for key=[%s]" %
-                                    key_name)
-                    return user_data
-
-                user_data = UserData(**prop_values)
-                # Both claim_username and set_password updates user_data
-                # and will call put() for us.
-                
-                return user_data
-
-            user_data = transaction_util.ensure_in_transaction(create_txn,
-                                                               xg_on=True)
-
-        else:
-            # No username means we don't have to do manual transactions.
-            # Note that get_or_insert is a transaction itself, and it can't
-            # be nested in the above transaction.
-            user_data = UserData.get_or_insert(**prop_values)
-            user_data = db.get(user_data.key())   # force-commit for HRD data
-
-        if user_data and not user_data.is_phantom:
-            # Record that we now have one more registered user
-            if (datetime.datetime.now() - user_data.joined).seconds < 60:
-                # Extra safety check against user_data.joined in case some
-                # subtle bug results in lots of calls to insert_for for
-                # UserData objects with existing key_names.
-                user_counter.add(1)
+        # No username means we don't have to do manual transactions.
+        # Note that get_or_insert is a transaction itself, and it can't
+        # be nested in the above transaction.
+        user_data = UserData.get_or_insert(**prop_values)
+        user_data = db.get(user_data.key())   # force-commit for HRD data
 
         return user_data
+
+    @classmethod
+    def key_for(cls, user_id):
+        return "user_id_key_%s" % user_id
